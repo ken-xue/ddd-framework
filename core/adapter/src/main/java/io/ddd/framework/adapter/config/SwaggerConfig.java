5172873ec80fa1e3c59fe598ee60b5c86a6c0c24
mikey <mikey@91zhen.com>
2022-04-01 16:10:46 +0800

    fix:fix swagger group

--- core/adapter/src/main/java/io/ddd/framework/adapter/config/SwaggerConfig.java
+++ core/adapter/src/main/java/io/ddd/framework/adapter/config/SwaggerConfig.java
@@ -19,27 +19,27 @@ import java.util.List;
 @EnableSwagger2
 @ConditionalOnProperty(name = "swagger.enable", havingValue = "true")
 public class SwaggerConfig {
-    // 设置默认TOKEN，方便测试
-    private static final String TOKEN = "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ6aGFveGluZ3VvLVtST0xFX0FETUlOLCBBVVRIX1dSSVRFXSIsImV4cCI6MTUzOTMzOTM0NX0.P9dkLQ7lpNODJppHBM-InSS90nw0XJieK8QNlZM0TeuNNQ8sUPYH-uif099A1-P2Ap6b_9lCLbXL2iR0OLdFyw";
-    @Bean
-    public Docket api() {
-        ParameterBuilder tokenPar = new ParameterBuilder();
-        List<Parameter> pars = new ArrayList<Parameter>();
-        tokenPar.name("Authorization").description("令牌").defaultValue(TOKEN).modelRef(new ModelRef("string")).parameterType("header").required(false).build();
-        pars.add(tokenPar.build());
-        Docket docket = new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("io.ddd.framework.adapter.rest"))
-                .paths(PathSelectors.ant("/**"))
-                .build()
-                .globalOperationParameters(pars)
-                .apiInfo(apiInfo())
-                .useDefaultResponseMessages(false)
-                .globalResponseMessage(RequestMethod.GET, Arrays.asList(
-                        new ResponseMessageBuilder().code(500).message("500 queue").responseModel(new ModelRef("Error")).build(),
-                        new ResponseMessageBuilder().code(403).message("Forbidden").build()));
-        return docket;
-    }
+//     设置默认TOKEN，方便测试
+//    private static final String TOKEN = "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ6aGFveGluZ3VvLVtST0xFX0FETUlOLCBBVVRIX1dSSVRFXSIsImV4cCI6MTUzOTMzOTM0NX0.P9dkLQ7lpNODJppHBM-InSS90nw0XJieK8QNlZM0TeuNNQ8sUPYH-uif099A1-P2Ap6b_9lCLbXL2iR0OLdFyw";
+//    @Bean
+//    public Docket api() {
+//        ParameterBuilder tokenPar = new ParameterBuilder();
+//        List<Parameter> pars = new ArrayList<Parameter>();
+//        tokenPar.name("Authorization").description("令牌").defaultValue(TOKEN).modelRef(new ModelRef("string")).parameterType("header").required(false).build();
+//        pars.add(tokenPar.build());
+//        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("io.ddd.framework.adapter.rest"))
+//                .paths(PathSelectors.ant("/**"))
+//                .build()
+//                .globalOperationParameters(pars)
+//                .apiInfo(apiInfo())
+//                .useDefaultResponseMessages(false)
+//                .globalResponseMessage(RequestMethod.GET, Arrays.asList(
+//                        new ResponseMessageBuilder().code(500).message("500 queue").responseModel(new ModelRef("Error")).build(),
+//                        new ResponseMessageBuilder().code(403).message("Forbidden").build()));
+//        return docket;
+//    }
 
     @Bean
     public Docket createRestApiForSys() {
