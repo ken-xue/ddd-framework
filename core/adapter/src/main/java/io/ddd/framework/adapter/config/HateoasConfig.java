e9f320239abf30c581ebac460d605a8434389528
mikey <mikey@91zhen.com>
2022-07-20 11:00:18 +0800

    feat:hateoas

---
+++ core/adapter/src/main/java/io/ddd/framework/adapter/config/HateoasConfig.java
@@ -0,0 +1,21 @@
+package io.ddd.framework.adapter.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class HateoasConfig {
+    @Bean
+    public LinkDiscoverers discoverers() {
+        List<LinkDiscoverer> plugins = new ArrayList<>();
+        plugins.add(new CollectionJsonLinkDiscoverer());
+        return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+    }
+}
--- core/core-client/src/main/java/io/ddd/framework/coreclient/dto/common/command/DTO.java
+++ core/core-client/src/main/java/io/ddd/framework/coreclient/dto/common/command/DTO.java
@@ -1,8 +1,9 @@
 package io.ddd.framework.coreclient.dto.common.command;
 
+//import org.springframework.hateoas.RepresentationModel;
 import java.io.Serializable;
 
-public abstract class DTO implements Serializable {
+public abstract class DTO /* extends RepresentationModel*/ implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
